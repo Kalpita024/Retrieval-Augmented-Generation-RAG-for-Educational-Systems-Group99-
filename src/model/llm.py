0e39d96219c2615342b3809082a9915ff3c9fcd5
aavp268-tech <aavp268@gmail.com>
2026-07-06 19:37:59 +0530

    main

--- src/model/llm.py
+++ src/model/llm.py
@@ -1,17 +1,15 @@
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
-import config.settings 
-
-LLM_MODEL = config.settings.LLM_MODEL
-TEMPERATURE = config.settings.TEMPERATURE
+from src.config.setting import LLM_MODEL, TEMPERATURE
 
 load_dotenv()
 
+
 def get_llm(temperature=TEMPERATURE, max_tokens=512):
-    llm= ChatGroq(
-        model= LLM_MODEL,
-        temperature=TEMPERATURE,
-        max_tokens=512,
+    llm = ChatGroq(
+        model=LLM_MODEL,
+        temperature=temperature,
+        max_tokens=max_tokens,
     )
-    
+
     return llm
